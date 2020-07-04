2a847db120cfe5705e70e2fd8403f440c2efa2d4
shehab.nagy1998@gmail.com <shehabnagy1998>
2020-07-04 22:07:38 +0200

    added build folder

---
+++ build/precache-manifest.f13fc0c052d0d54ca67c902d2d4e10a7.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04065c8b19aab255d334b53432ddd241",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9d2971a2f25de14748f",
+    "url": "/static/css/2.364eb9c8.chunk.css"
+  },
+  {
+    "revision": "cf8b555f4451224a3a5b",
+    "url": "/static/css/main.bf033fdb.chunk.css"
+  },
+  {
+    "revision": "f9d2971a2f25de14748f",
+    "url": "/static/js/2.b75eef3f.chunk.js"
+  },
+  {
+    "revision": "122d9b8c78ded30a7ed2361f1a4937a8",
+    "url": "/static/js/2.b75eef3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf8b555f4451224a3a5b",
+    "url": "/static/js/main.0891f46b.chunk.js"
+  },
+  {
+    "revision": "cced8e9c2c1bb34d1210",
+    "url": "/static/js/runtime-main.b11bc7a8.js"
+  },
+  {
+    "revision": "08df9f7c2324ffe4b7f718d014488002",
+    "url": "/static/media/el-shorouk.08df9f7c.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "/static/media/fontawesome-webfont.acf3dcb7.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "3e843205d26e4e1c9566e52661cf9e58",
+    "url": "/static/media/no-pic.3e843205.png"
+  },
+  {
+    "revision": "32523e3a234e799c797041a2b892dc25",
+    "url": "/static/media/no-pic2.32523e3a.png"
+  },
+  {
+    "revision": "6dbfbd4139804b764d667e655160ab89",
+    "url": "/static/media/pic.6dbfbd41.jpg"
+  }
+]);
