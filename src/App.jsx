31822e00ab1067cb9962b715b52cf8c138ce69e1
shehab.nagy1998@gmail.com <shehabnagy1998>
2020-07-04 21:23:30 +0200

    changed API links

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,11 @@ import { Switch, Route, useHistory, Redirect } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import { connect } from "react-redux";
 import PageLogin from "./components/Login/PageLogin";
-import { REDUX_BROWSE_HISTORY, CDN, REDUX_SOCKET } from "./store/CONSTANTS";
+import {
+  REDUX_BROWSE_HISTORY,
+  REDUX_SOCKET,
+  SOCKET_API,
+} from "./store/CONSTANTS";
 import PageDashboard from "./components/Dashboard/PageDashboard";
 import PageSpinner from "./components/Loaders/PageSpinner";
 import socketIOClient from "socket.io-client";
@@ -13,7 +17,7 @@ function App({ pageLoaders, browseHistorySet, userDetails, socketSet }) {
   const browseHistory = useHistory();
   useEffect(() => {
     browseHistorySet(browseHistory);
-    const socket = socketIOClient(CDN);
+    const socket = socketIOClient(SOCKET_API);
     socket.on("NOTIFICATION", (data) => {
       console.log(data);
     });
